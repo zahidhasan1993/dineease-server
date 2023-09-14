74acf35c316b4b1b61e58f1b9499749a58a8e17e
devZahid <officialzahidhasan19932021@gmail.com>
2023-09-14 14:17:49 +0600

    adding route to save user to own database

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.uoombu0.mongodb.net/?retryWrites=true&w=majority`;
-
 
 //middle ware
 
@@ -13,16 +12,13 @@ app.use(cors());
 app.use(express.json());
 //db connection
 
-
-
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,57 +26,70 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
     // db-access
     const database = client.db("dineease");
     const menuCollection = database.collection("menu");
     const reviewCollection = database.collection("reviews");
     const cartCollection = database.collection("carts");
-
+    const userCollection = database.collection("users");
 
     // route connections
 
-
     // get apis
-    app.get('/menu', async(req,res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/reviews', async(req,res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/carts', async(req,res) => {
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
       // console.log(email);
-      const query = {userEmail : email}
+      const query = { userEmail: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
-
+    });
 
     // post apis
 
-    app.post('/carts', async(req,res)=> {
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const email = user.email;
+      const query = { email: email };
+      const existingUser = await userCollection.findOne(query);
+      if (!existingUser) {
+        const result = await userCollection.insertOne(user);
+
+        res.send(result);
+      } else {
+        console.log("user already exists");
+      }
+    });
 
     //delete apis
 
-    app.delete('/carts/:id', async(req,res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
 
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -88,15 +97,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 //connection routes
 
-app.get('/', (req,res) => {
-    res.send('WELCOME TO DINEEASE SERVER')
-})
+app.get("/", (req, res) => {
+  res.send("WELCOME TO DINEEASE SERVER");
+});
 
 //checking connection
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
